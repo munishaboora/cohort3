a5d2d46baacba35182bb5d341c9372c23d3c8d29
munisha <munishaboora@hotmail.co.uk>
2021-10-19 10:47:24 +0100

    day 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Main {
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Munisha";
+        int age = 20;
+        long age2 = 20L;
+
+        BigDecimal decimal = new BigDecimal("10.00");
+
+        byte n = 127;
+
+        double pi = 3.14;
+        float pi2 = 3.14F;
+
+        boolean isAdult = false;
+
+        char letter = 'A';
+
+        //
+        System.out.println(1 + 1);
+        System.out.println(1 * 2);
+        System.out.println(1 / 2);
+        System.out.println(3 - 2);
+        System.out.println(10 % 3 == 0);
+        System.out.println(10 == 10);
+        System.out.println(10 != 10);
+
+        String name2 = "Helena";
+        String name3 = "Helena";
+        System.out.println(name2 == name3);
+        System.out.println(name == name3); //false
+
+        //To compare strings:
+        System.out.println(name.compareTo(name2));
+        System.out.println(name.equals(name2));
+
+
+
+    }
+
 }
---
+++ src/main/java/Sub.java
@@ -0,0 +1,9 @@
+public class Sub {
+    public static void main(String[] args) {
+        //
+
+        Point a = new Point(10,10);
+        Point b = a;
+        b.x = 20;
+    }
+}
